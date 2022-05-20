be4b7073b5075f5a4a65403a4cd2df484d0a07e9
standardforever <standard.forever123@gmail.com>
2022-05-20 08:30:57 +0000

    updated

--- main.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-	_printf("let, %s, with, %c\", littl %s, so that %s count %c", "start", 'a', "discussion", "we", 'a');
-
-	return (0);
-}
--- printf.c
+++ printf.c
@@ -1,125 +1,43 @@
 #include "main.h"
 
 /**
- * _alloc_mem - it allocate a memory using malloc
- * @count: the total of of count of the parametr.
- * Return: pointer to the allocated memory
- */
-
-char *_alloc_mem(int count)
-{
-	char *str = malloc(count * sizeof(int));
-
-	if (str == NULL)
-		return ('\0');
-	return (str);
-}
-
-/**
- * _count - it count the total nubmers of character given
- * @format: is a pointer that point to the string to be counted
- * @args: it takes va_list of
- * Return: an int of the count
- */
-/*int _count(const char *format, va_list args)
-{
-
-	int state = 0, count = 0;
-
-	const char *s;
-	while (*format)
-	{
-		if (state == 0)
-		{
-			if (*format == '%')
-				state = 1;
-		}
-		else if (state == 1)
-		{
-			switch(*format)
-			{
-				case 'c':
-					count--;
-					break;
-				case 's':
-					//s = va_arg(args, const char *);
-					//while (*s)
-					//	*s++, count++;
-					break;
-			}
-		}
-		else
-			count++;
-		format++;
-	}
-
-
-	int i, j, count = 0;
-	const char *s;
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%' && format[i + 1] == 'c')
-			count++, i++;
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			s = va_arg(args, const char *);
-			printf("%s", s);
-			j = 0;
-			//while (s[j])
-			//	count++, j++;
-			i++;
-		}
-		else
-			count++;
-	}
-	return (count++);
-}*/
-/**
- * _printf - it print an input
- * @format: it's a pointer to a string
- * Description:  it takes a string and check if it sees '%s' or '%c, if yes
- *               it takes the matched argumetn from arg_list and print it.
- * Return: it returns the string pointered to by format.
+ * _printf - it print anything
+ * @format: the string to be printed
+ * Return: integer
  */
 
 int _printf(const char *format, ...)
 {
-	int i = 0, k = 0, j, count;
-	const char *s;
-	char *str;
-	va_list arg;
+	char c;
+	char const *str;
+	int i, j;
+	va_list args;
 
-	str = _alloc_mem(100);
-	va_start(arg, format), count = 0;
+	va_start(args, format);
 	for (i = 0; format[i]; i++)
 	{
-		if (format[i] == '%' && format[i + 1] == '\0')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
-			str[k] = format[i];
-			k++, count++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			str[k] = va_arg(arg, int);
-			i++, k++, count++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			s = va_arg(arg, const char *);
-			while (s[j])
+			switch (format[i + 1])
 			{
-				str[k] = s[j];
-				 k++, j++, count++;
+				case 'c':
+					c = va_arg(args, int);
+					write(1, &c, 1);
+					i++;
+					break;
+				case 's':
+					str = va_arg(args, const char *);
+					for (j = 0; str[j]; j++)
+						;
+					write(1, str, j);
+					i++;
+					break;
+				default:
+					write(1, &format[i], 1);
 			}
-			k++, i++, count++, j = 0;
 		}
 		else
-		{
-			str[k] = format[i];
-			 count++, k++;
-		}
+			write(1, &format[i], 1);
 	}
-	va_end(arg);
-	return (write(1, str, count));
+	return (0);
 }
