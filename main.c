ce30bea0e7e12f5cfbc00964cb02a2f88abd876b
standardforever <standard.forever123@gmail.com>
2022-05-20 17:35:02 +0000

    updated

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+	 _printf("abudul is a grown up boy bougth very sturborn and  %s we %s, %s%c, let shall see %s %s %s","shoould",  "string", "str", 'c', "str", "string", "stop");
+	_printf("%s %s", "start", "stsl");
+	return (0);
+}
