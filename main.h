93488b7c5c5ed4bb5dfcae68b81923b8dbab0371
standardforever <standard.forever123@gmail.com>
2022-05-21 04:00:06 +0000

    updated

--- main.h
+++ main.h
@@ -7,6 +7,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 int _printf(const char *format, ...);
+int _print(const char *format, va_list args);
 int _putchar(char c);
 int _count_char(const char *format, va_list arg);
 int _count_str(const char *format, va_list arg);
---
+++ printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _printf - it print an input
+ * @format: it's a pointer to a string
+ * Description:  it takes a string and check if it sees '%s' or '%c, if yes
+ *               it takes the matched argumetn from arg_list and print it.
+ * Return: it returns the string pointered to by format.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list chara;
+	int count;
+
+	va_start(chara, format);
+	count = _print(format, chara);
+	va_end(chara);
+	return (count);
+}
